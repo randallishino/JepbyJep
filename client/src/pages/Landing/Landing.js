fe59df155d4f277884e5e24c690d1414fa12606c
Nick <nlweber3@gmail.com>
2018-04-30 19:48:18 -0700

    styling

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,45 +1,35 @@
-import React, {Component } from "react";
+import React, { Component } from "react";
 import Wrapper from "../../components/Wrapper";
 import video from "../../compressed.mp4";
 import "./Landing.css";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 class Landing extends Component {
-constructor (props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-        videoURL: video
+        this.state = {
+            videoURL: video
+        }
     }
-}
 
-render () {
-    return (
-      <Wrapper>
-  <NavLink to="/home" className="grow" id="title">JEPbyJEP</NavLink>
+    render() {
+        return (
+            <Wrapper>
 
-        <video id="myVideo" loop autoPlay muted>
-            <source src={this.state.videoURL} type="video/mp4" />
-        </video>
+                <div className="title_jbj">
+                    <NavLink to="/home" className="grow" id="title">JEPbyJEP</NavLink>
+                </div>
 
-<div className="navs">
-<ul className="nav justify content center" id="navs">
-<li className="nav-item">
-<NavLink to="/contact" className="nav-link">CONTACT</NavLink>
-</li>
-<li className="nav-item">
-  <NavLink to="/films" className="nav-link">FILMS</NavLink>
-</li>
-<li className="nav-item">
-<NavLink to="/clothes" className="nav-link">CLOTHES</NavLink>
-</li>
-</ul>
+                <video id="myVideo" loop autoPlay muted>
+                    <source src={this.state.videoURL} type="video/mp4" />
+                </video>
 
-</div>
-</Wrapper>
-    
-    )
-}
+
+            </Wrapper>
+
+        )
+    }
 };
 
 export default Landing;

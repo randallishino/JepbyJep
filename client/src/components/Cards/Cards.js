8cc3502775329c85b68448425953004010d0e7df
randall ishino <rishino24@gmail.com>
2018-05-07 21:59:29 -0700

    cards working with shopify data

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
- CardSubtitle, CardBody } from 'reactstrap';
- import "./Cards.css";
+// import "./Cards.css";
 
 
 const Cards = (props) => {
   return (
-  <div className="cards">
-    <CardDeck>
-      <Card>
-        <CardImg top width="100%" src={props.image} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </CardDeck>
-  </div>
-  );
-};
+<div className="card">
+<div className="img-container">
+    <img alt ={props.name} src={props.image} />
+</div>
+<div className="content">
+    <ul>
+        <strong>{props.title}</strong>
+    </ul>
+    <ul>
+        <strong>{props.price}</strong>
+    </ul>
+    <ul>
+        <strong> {props.description}</strong>
+    </ul>
+</div>
+</div>
+  )
+}
 
-export default Card;
+export default Cards;

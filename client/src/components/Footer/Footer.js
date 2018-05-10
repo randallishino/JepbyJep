896eac01dd1d7999c412f6ed3d6cdfdba8d7c3b2
dkwazhere <dhealth77@gmail.com>
2018-05-10 01:22:56 -0700

    changed twitter font color

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
     <div className="navbar">
         <div className="row">
         <i className="fab fa-facebook"></i>
-        <a href="https://www.instagram.com/jepbyjep.space/"><i style={{color:"grey"}}  id="instagram" className="fab fa-instagram"></i></a>
-        <a href="https://twitter.com/jepbyjep"><i style={{color:"#325F15"}} id="twitter" className="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/jepbyjep.space/"><i style={{color:"#383736"}}  id="instagram" className="fab fa-instagram"></i></a>
+        <a href="https://twitter.com/jepbyjep"><i style={{color:"#383736"}} id="twitter" className="fab fa-twitter"></i></a>
         <div className="login-button">
         </div>
         </div>
